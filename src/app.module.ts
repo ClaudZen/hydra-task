0e57b650f0f8cb20e6f278779c6e786fb4ea321a
ClaudZen <claudio.anticoi@outlook.es>
2022-08-21 13:03:04 +0000

    feature init proyect module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ProyectModule } from './proyect/proyect.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
     DatabaseModule,
+    ProyectModule,
   ],
   controllers: [],
   providers: [],
