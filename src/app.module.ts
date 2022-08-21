23c577178d19330d723c1b14e22b5fed0bb66187
ClaudZen <claudio.anticoi@outlook.es>
2022-08-21 07:28:10 +0000

    feature connection bd config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MssqlConfig } from './config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+    TypeOrmModule.forRootAsync({ useClass: MssqlConfig }),
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/config/mssql.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class MssqlConfig implements TypeOrmOptionsFactory {
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mssql',
+      host: process.env.MSSQL_HOST,
+      port: Number(process.env.MSSQL_PORT),
+      username: process.env.MSSQL_USERNAME,
+      password: process.env.MSSQL_PASSWORD,
+      database: process.env.MSSQL_DATABASE,
+      entities: ['**/*.entity{.ts,.js}'],
+      synchronize: true,
+      extra: {
+        trustServerCertificate: true,
+      },
+    };
+  }
+}
